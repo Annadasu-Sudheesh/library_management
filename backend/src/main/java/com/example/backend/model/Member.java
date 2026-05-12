c600383cac65eb45df62b161b8d29db5eb6c5a1f
sudheesh <sudheeshannadasu0672@gmail.com>
2026-05-12 13:41:47 +0530

    Updated backend

--- backend/src/main/java/com/example/backend/model/Member.java
+++ backend/src/main/java/com/example/backend/model/Member.java
@@ -1,40 +1,33 @@
-
 package com.example.backend.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import java.util.ArrayList;
-import java.util.List;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
 
 @Entity
 @Table(name = "members")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
-@EqualsAndHashCode(exclude = "issuesRecords")
-@ToString(exclude = "issuesRecords")
 public class Member {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long memberId;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    private String name;
+	@NotBlank
+	private String name;
 
-    private String email;
+	@NotBlank
+	private String email;
 
-    @OneToMany(mappedBy = "member")
-    @Builder.Default
-    private List<IssuesRecord> issuesRecords = new ArrayList<>();
+	@NotNull
+	private Boolean active = Boolean.TRUE;
 }
